42b1a82c14f23a1d810fddc3b82d18b2a75acd70
vladbelukha <vlad.beluha@gmail.com>
2025-03-21 13:13:30 -0700

    summary preparation

---
+++ drr/src/UI/src/app/drif/drif-project/drif-forecast/drif-forecast-summary/drif-forecast-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'drif-forecast-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './drif-forecast-summary.component.html',
+  styleUrl: './drif-forecast-summary.component.scss',
+})
+export class DrifForecastSummaryComponent {}
